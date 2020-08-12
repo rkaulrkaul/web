8156c1c22775ef50b08cfe5885a40638fe8e1f2c
Rishab Kaul <rk493@cornell.edu>
2020-08-11 22:13:13 -0400

    scroll changes

---
+++ public/javascripts/scroll.js
@@ -0,0 +1,7 @@
+$('document').ready(function() {
+    $(document).scroll(function(){
+    if(document.documentElement.clientHeight + 
+    $(document).scrollTop() >= document.body.offsetHeight )$(document).scrollTop(0);
+    console.log("hi");
+    });
+ }); 
